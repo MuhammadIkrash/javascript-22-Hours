a206808539dd0ef6161012b1813da90297cdf8b2
Muhammad Ikrash <mikrash62@gmail.com>
2026-06-01 21:03:38 +0500

    More Learning

---
+++ String/string.js
@@ -0,0 +1,41 @@
+console.log('string');
+
+// string is use for store text value .
+
+//for example : 
+
+let a = 'hello' // output : hello
+
+// you can write string in double quote  single quote . there is no different between them .
+
+// Example : 
+
+console.log('hello World'); // output : hello World
+
+console.log("hello World"); // output : hello World
+
+
+// Template Strings are use to combine tow string or variable 
+
+// example :
+
+let a = 2;
+
+console.log(`I am ${a} Year old`);  // output : I am 2 Year old 
+
+// Escape sequences : 
+
+//  "+++ /' +++" :
+
+console.log("i am \'pakistani"); // out : i am ' pakistani 
+
+//  "+++ /" +++" :
+
+console.log('i am \"pakistani'); // out : i am " pakistani 
+
+//  "+++ /' +++" :
+
+console.log("i am \npakistani");
+// out :
+//  i am '
+//  pakistani
