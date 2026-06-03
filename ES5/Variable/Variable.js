cab11f18b777d7b4840b123d3172f22af685c8db
Muhammad Ikrash <mikrash62@gmail.com>
2026-06-03 15:25:40 +0500

    more learning

---
+++ ES5/Variable/Variable.js
@@ -0,0 +1,67 @@
+// variable == Data Container
+
+// variable store the data , it can be any data type .
+// there a four way to declared variable .
+
+// Modern Javascript
+// let a = 1 ; // let cannot be redeclared within the same scope .
+// a = 2 // now let value is 2 Because let can be reinitialized
+// const b = 1;  let cannot be redeclared within the same scope .
+// b = 2 // reinitialized not allow in const
+
+// old Javascript (Not Recommended)
+// var a = 1 ; // let can be redeclared within the same scope .
+// a = 2 // now let value is 2 Because var can be reinitialized
+
+// automatically variable declaration  (Not Recommended)
+// a = 2;
+
+
+/* JavaScript Identifiers
+Variables are identified with unique names called identifiers.
+
+Names can be short like x, y, z.
+
+Names can be descriptive age, sum, carName.
+
+The rules for constructing names (identifiers) are:
+
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter, a $ sign or an underscore (_).
+Names are case sensitive (X is different from x).
+Reserved words (JavaScript keywords) cannot be used as names. */
+// Numbers are not allowed as the first character in names.
+
+// The Assignment Operator
+// In JavaScript, the equal sign (=) is an assignment operator, not an equal to operator.
+
+// This is different from algebra. The following does not make sense in algebra:
+
+// x = x + 5
+// In JavaScript, however, it makes perfect sense: it assigns the value of x + 5 to x.
+
+// (It calculates the value of x + 5 and puts the result into x. The value of x is incremented by 5.)
+
+// The equal to operator is written like == in JavaScript.
+
+// JavaScript Arithmetic
+// As with algebra, you can do arithmetic with JavaScript variables, using operators like = and +:
+
+// Example
+// let x = 5 + 2 + 3;
+// You can also add strings, but strings will be concatenated:
+
+// Example
+// let x = "John" + " " + "Doe";
+
+// If you put a number in quotes, the rest of the numbers will be treated as strings, and concatenated.
+
+// Examples
+// let x = "5" + 2 + 3;
+// let x = 2 + 3 + "5";
+
+
+// Variable Practice 
+
+// cart_function  (html file)
+// calculator  (html file)
